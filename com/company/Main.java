27d055b93743150564ca281be657548f8b7f8cdd
ctambaro <tambaro123@gmail.com>
2020-09-04 15:57:04 -0400

    All files

---
+++ com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int radius;
+	    float area;
+	    radius = 20;
+	    area = (float) (radius * radius * 3.14159);
+        System.out.println("Area equals " + area);
+    }
+}
