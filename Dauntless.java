85537fb5a6c8a1b6efa1344f3e5656f58a67e52e
ctambaro <tambaro123@gmail.com>
2020-10-19 22:14:31 -0400

    dauntless

--- Dauntless.java
+++ Dauntless.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Dauntless {
+public class Dauntless extends Tank{
+    public Dauntless(String[] fields) {
+        super(fields);
+    }
+
+    public static boolean meetsConditions(String[] fields){
+        return Integer.parseInt(fields[4]) == 100;
+    }
+
+    public int attack(SuperHero oHero){
+        int damage = super.attack(oHero);
+
+        damage += this.getDurability() * 2.0 - oHero.getStrength();
+
+        return damage;
+    }
+
+
+
 }
