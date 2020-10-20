06642b82d55ec60079a3d7883ee7b1e52bff5f25
ctambaro <tambaro123@gmail.com>
2020-10-19 22:11:04 -0400

    brute--

--- Savage.java
+++ Savage.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Savage {
+public class Savage extends Brute{
+    public Savage(String[] fields) {
+        super(fields);
+    }
+
+    public static boolean meetsConditions(String[] fields){
+        return Integer.parseInt(fields[2])- Integer.parseInt(fields[1]) > 65;
+    }
+
+    public int attack(SuperHero oHero){
+        int damage = super.attack(oHero);
+
+        damage += this.getStrength() - oHero.getStrength();
+
+        return damage;
+    }
+
+
+
 }
