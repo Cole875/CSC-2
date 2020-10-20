aeb2cf09744db114f7054ecba14b414b4c57e640
ctambaro <tambaro123@gmail.com>
2020-10-19 22:13:06 -0400

    tank

--- Tank.java
+++ Tank.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Tank {
+public class Tank extends SuperHero {
+    public Tank(String[] fields) {
+        super(fields);
+    }
+
+    public static boolean meetsConditions(String[] fields){
+        return Integer.parseInt(fields[4]) > 20;
+    }
+
+    public int attack(SuperHero oHero){
+        int damage = super.attack(oHero);
+
+        damage += this.getDurability() * 1.5;
+
+        return damage;
+    }
 }
