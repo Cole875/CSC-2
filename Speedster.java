002401181a8573fa19b39483753072cdbfeb4b63
ctambaro <tambaro123@gmail.com>
2020-10-19 22:13:54 -0400

    speedster

--- Speedster.java
+++ Speedster.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Speedster {
+public class Speedster extends SuperHero {
+    public Speedster(String[] fields) {
+        super(fields);
+    }
+
+    public static boolean meetsConditions(String[] fields){
+        return Integer.parseInt(fields[3]) > 90;
+    }
+
+    public int attack(SuperHero oHero){
+        int damage = super.attack(oHero);
+
+        damage += this.getSpeed() * 1.25 - oHero.getSpeed();
+
+        return damage;
+    }
+
 }
