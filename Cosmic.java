ceb6e08582bf1a5a7b641087a5fba4eccf29353f
ctambaro <tambaro123@gmail.com>
2020-10-19 22:12:15 -0400

    cosmic

--- Cosmic.java
+++ Cosmic.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Cosmic {
+public class Cosmic extends SuperHero {
+    public Cosmic(String[] fields) {
+        super(fields);
+    }
+
+    public static boolean meetsConditions(String[] fields){
+        return Integer.parseInt(fields[5]) == 100;
+    }
+
+    public int attack(SuperHero oHero){
+        int damage = super.attack(oHero);
+
+        damage += this.getPower() * 2.0 - (oHero.getPower())/2;
+
+        return damage;
+    }
+
+
 }
